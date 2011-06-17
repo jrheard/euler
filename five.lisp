f55010cfe73ccc04e28f901a2326799583efe7f3
jrheard <jrheard@cs.stanford.edu>
2011-06-17 00:08:14 -0700

    transform a list of factors into an alist of factors -> powers

--- five.lisp
+++ five.lisp
@@ -20,3 +20,18 @@
 				 (let ((factor (find-factor n 2)))
 					  (f (/ n factor) (cons factor factors))))))
 		   (f num '())))
+
+(defun factors-and-powers (num)
+  (labels ((f (facts alist)
+			 (if facts
+				 (progn
+				   (if (assoc (car facts) alist) ; TODO is there a better way to express this?
+					   (incf (cdr (assoc (car facts) alist))) ; TODO this feels gross
+					   (push (cons (car facts) 1) alist))
+					   (f (cdr facts) alist))
+				 alist)))
+		  (f (factors num) '())))
+
+(defparameter *divisors* (loop for i from 1 to 20 collect i))
+
+; TODO implement the algorithm that actually solves the question that this problem asks
