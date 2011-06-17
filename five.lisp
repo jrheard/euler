94357577456d57eda55dd47b07c93e98671ae132
jrheard <jrheard@cs.stanford.edu>
2011-06-17 09:55:47 -0700

    don't need this any more

--- five.lisp
+++ five.lisp
@@ -2,13 +2,6 @@
 ;
 ; What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
-(defun divisible-by-1-to-20p (num)
-  (let ((divisible t))
-	   (loop for i from 1 to 20
-			when (not (zerop (mod num i)))
-			do (setf divisible nil))
-	   divisible))
-
 (defun factors (num)
   (labels ((find-factor (n cur)
 					   (if (zerop (mod n cur))
