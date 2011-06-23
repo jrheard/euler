3ac65049a2622312c504c4870e2b18ef184ba56d
jrheard <jrheard@cs.stanford.edu>
2011-06-22 22:10:17 -0700

    add fourteen. this one was interesting. solution runtime butts right up against the one-minute cap. wonder how i can refine this.

--- fourteen.lisp
+++ fourteen.lisp
@@ -9,3 +9,20 @@
 				 (cons num acc)
 				 (f (hotpo num) (cons num acc)))))
 		  (reverse (f start '()))))
+
+(defun find-longest-hailstone (maximum)
+  (let ((seen (make-hash-table)))
+	   (labels ((f (i longest-start longest-chain)
+				  (if (eq i 1)
+					  longest-start
+					  (if (gethash i seen)
+						  (f (1- i) longest-start longest-chain)
+						  (let ((seq (hailstone i)))
+							   (if (> (length seq) longest-chain)
+									 (progn
+									   (mapcan (lambda (term) (setf (gethash term seen) t)) seq)
+									   (f (1- i) i (length seq)))
+								   (f (1- i) longest-start longest-chain)))))))
+			   (f maximum 0 0))))
+
+(print (find-longest-hailstone 1000000))
