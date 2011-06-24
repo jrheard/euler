ea622a648ee0cd0ec096addbd8b0e9328f1dfe55
jrheard <jrheard@cs.stanford.edu>
2011-06-24 09:40:59 -0700

    hrm. way simpler and way faster. no memoizing.

--- twelve.lisp
+++ twelve.lisp
@@ -1,15 +1,11 @@
-(let ((seen (make-hash-table)))
-	 (defun divisors (num)
-	   (labels ((f (divisor divs)
-				  (if (<= divisor 1)
-					  (append divs '(1))
-					  (if (eq (mod num divisor) 0)
-						  (let ((val (gethash divisor seen)))
-								(if val
-									(append val divs)
-									(f (1- divisor) (cons divisor divs))))
-						  (f (1- divisor) divs)))))
-			   (setf (gethash num seen) (f (1- num) (list num))))))
+(defun divisors (num)
+  (labels ((f (i divs)
+		   (if (> i (/ num 2))
+			   divs
+			   (if (eq (mod num i) 0)
+				   (f (1+ i) (append (list i (/ num i)) divs))
+				   (f (1+ i) divs)))))
+	   (remove-duplicates (f 1 '()))))
 
 (defun first-triangle-number-with-over-num-divisors (num)
   (labels ((f (triangle i)
@@ -18,4 +14,19 @@
 				 (f (+ triangle i) (1+ i)))))
 		  (f 1 2)))
 
-(print (first-triangle-number-with-over-num-divisors 10))
+(time (first-triangle-number-with-over-num-divisors 60))
+
+
+; time for first-triangle of 60 in current version
+; Real time: 0.501909 sec.
+; Run time: 0.463359 sec.
+; Space: 7619292 Bytes
+; GC: 14, GC time: 0.044082 sec.
+
+
+
+
+;time for first-triangle of 60 in old version
+; Real time: 15.30771 sec.
+; Run time: 13.309559 sec.
+; Space: 255540 Bytes
