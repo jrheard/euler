1959b8715791ae407e454b9779c1f18f9010dd05
jrheard <jrheard@cs.stanford.edu>
2011-06-26 19:29:17 -0700

    sweet. studied the grid for a long time, found a pattern, implemented the pattern, runs hell of fast. cool.

--- fifteen.lisp
+++ fifteen.lisp
@@ -1,14 +1,13 @@
 (defun routes-through-a-grid (x y)
-  (let ((choices 0))
-	   (labels ((f (xx yy direction)
-				  (let ((had-a-choice (and (not (eq xx x)) (not (eq yy y)))))
-						(when (not (eq xx x))
-							  (when (and had-a-choice (not (eq direction 'x))) (incf choices))
-							  (f (1+ xx) yy 'x))
-						(when (not (eq yy y))
-							  (when (and had-a-choice (not (eq direction 'y))) (incf choices))
-							  (f xx (1+ yy) 'y)))))
-			   (f 0 0 nil))
-	   choices))
+  (let ((choices (make-hash-table :test #'equal)))
+	   (setf (gethash '(1 . 1) choices) 1)
+	   (loop for yy from 1 to y
+			do (loop for xx from 1 to x
+					when (not (and (eq xx 1) (eq yy 1)))
+					do (setf (gethash (cons xx yy) choices)
+							 (+ (or (gethash (cons (1- xx) yy) choices) 0)
+								(or (gethash (cons xx (1- yy)) choices) 0)))))
+	   (incf (gethash '(1 . 1) choices)) ; (1, 1) always has value 2, but calculations are performed initially as if it had value 1
+	   (reduce #'+ (loop for val being the hash-values of choices collect val))))
 
 (time (print (routes-through-a-grid 20 20)))
