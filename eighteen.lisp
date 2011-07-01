8250a6bb6bb14e7e288e0657d26bfc23b99051c0
jrheard <jrheard@cs.stanford.edu>
2011-07-01 00:16:06 -0700

    wip

--- eighteen.lisp
+++ eighteen.lisp
@@ -9,13 +9,13 @@
 						   while line collect (mapcar #'read-from-string (split-sequence:split-sequence #\Space line)))))))
 		 (make-array (length seq) :initial-contents seq)))
 
-(defun aref-safe (arr idx)
-  (if (< idx (length arr))
-	  (aref arr idx)
-	  0))
-
 (defun level (node)
-  (truncate (log (1+ node) 2)))
+  ; there must be a more efficient way to do this
+  (labels ((f (i acc)
+			 (if (<= (1+ node) acc)
+				 (1- i) ; "level in the tree" is indexed from 0
+				 (f (1+ i) (+ acc i)))))
+		  (f 1 1)))
 
 (defun left (tree node)
   (+ (1+ node) (level node)))
@@ -26,14 +26,16 @@
 (defun paths (tree)
   (let ((paths '()))
 	   (labels ((f (node acc)
-				  (if (and (<= (left tree node) (length tree)) (<= (right tree node) (length tree)))
+				  (if (and (< (left tree node) (length tree)) (< (right tree node) (length tree)))
 					  (progn
-						(f (left tree node) (cons (aref-safe tree (left tree node)) acc))
-						(f (right tree node) (cons (aref-safe tree (right tree node)) acc)))
-					  (push acc paths))))
+						(f (left tree node) (cons (aref tree (left tree node)) acc))
+						(f (right tree node) (cons (aref tree (right tree node)) acc)))
+					  (push (reverse acc) paths))))
 			   (f 0 (list (aref tree 0))))
 	   paths))
 
 (compile 'paths)
 
-(time (print (reduce #'max (mapcar (lambda (lst) (reduce #'+ lst)) (paths *tree*)))))
+;(time (print (reduce #'max (mapcar (lambda (lst) (reduce #'+ lst)) (paths *tree*)))))
+(print (length (paths *tree*)))
+;(print (paths *tree*))
