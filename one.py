c3c32c64854ef63324430c51ef76f906ee2b4bcb
jrheard <jrheard@cs.stanford.edu>
2011-08-25 22:35:18 -0700

    problem one in python

---
+++ one.py
@@ -0,0 +1,24 @@
+# verbose version
+multiples = []
+for i in range(1000):
+	if i % 3 == 0:
+		multiples.append(i)
+	elif i % 5 == 0:
+		multiples.append(i)
+
+print sum(multiples)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# one-liner version
+print sum(i for i in range(1000) if i % 3 == 0 or i % 5 == 0)
