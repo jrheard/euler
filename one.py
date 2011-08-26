19943bfe659e4dd93428c628127f2cb911b91789
jrheard <jrheard@cs.stanford.edu>
2011-08-26 09:09:19 -0700

    i am dumb

--- one.py
+++ one.py
@@ -1,9 +1,7 @@
 # verbose version
 multiples = []
 for i in range(1000):
-	if i % 3 == 0:
-		multiples.append(i)
-	elif i % 5 == 0:
+	if i % 3 == 0 or i % 5 == 0:
 		multiples.append(i)
 
 print sum(multiples)
