01b90592876c24afa505552edba86c833db9747c
jrheard <jrheard@cs.stanford.edu>
2012-02-07 00:38:36 -0800

    5.hs. yessssss

--- haskell/5.hs
+++ haskell/5.hs
@@ -1,15 +1,11 @@
+import Data.List (sort)
+import Data.List.Ordered (minus, union)
+
 -- 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 -- What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
 -- so this works, but (head nums_divisible_by_1_through_20) doesn't terminate in less than a minute.
 nums_divisible_by_1_through_20 = [x | x <- [1..], foldl (\acc y -> acc && (x `mod` y == 0)) True [1..20]]
-
-minus :: Ord a => [a] -> [a] -> [a]
-minus (x:xs) (y:ys) = case (compare x y) of
-    LT -> x : minus xs (y:ys)
-    EQ -> minus xs ys
-    GT -> minus (x:xs) ys
-minus xs _ = xs
 
 primesTo :: (Ord a, Num a, Enum a) => a -> [a]
 primesTo n = 2 : eratos [3,5..n] where
@@ -23,11 +19,15 @@ leastFactor 1 = 1
 leastFactor x = head [y | y <- (takeWhile (<= x) smallPrimes), x `mod` y == 0]
 
 primeFactors :: Integer -> [Integer]
-primeFactors x = (takeWhile (< x) (facts x [])) where
+primeFactors x = (takeWhile (<=x) (facts x [])) where
     facts 1 [] = []
     facts 1 (y:ys) = y:ys
     facts m lst = facts (m `div` (leastFactor m)) ((leastFactor m):lst)
 
-lcm :: Num a => [a] -> a
+lcm :: [Integer] -> Integer
 lcm [] = error "empty list"
---lcm (x:xs) = foo
+lcm (x:xs) = product (commonFactors (x:xs) []) where
+    commonFactors [] lst = lst
+    commonFactors (x:xs) factors = commonFactors xs (union (sort (primeFactors x)) factors)
+
+main = print (Main.lcm [1..20])
