451f768c510bb3b92ebd679df3e2462360491fec
jrheard <jrheard@cs.stanford.edu>
2012-04-14 17:37:27 -0700

    FINALLY finished 15. jesus christ i was so close that entire time

--- haskell/15.hs
+++ haskell/15.hs
@@ -1,26 +1,14 @@
 -- Starting in the top left corner of a 2x2 grid, there are 6 routes (without backtracking) to the bottom right corner.
 -- How many routes are there through a 20x20 grid?
 
-import Data.List (delete, nub, permutations)
+gridCounts x y = counts where
+    counts = [[gridVal xx yy | yy <- [0..y-1]] | xx <- [0..x-1]]
+    gridVal x y
+        | x == 0 && y == 0 = 2
+        | x == 0 || y == 0 = 1
+        | otherwise = ((counts !! (x - 1)) !! y) + ((counts !! x) !! (y - 1))
 
+numRoutes :: Int -> Int -> Int
+numRoutes x y = sum $ map (\x -> sum x) (gridCounts x y)
 
-data Direction = D | R deriving (Eq, Show)
-
-
-routes :: Int -> Int -> [[Int]]
-routes x y = nub $ permutations $ concat [(take x (repeat 0)), (take y (repeat 1))]
-
-routesLeft :: [Direction] -> [[Direction]]
-routesLeft remaining
-    | D `elem` remaining && R `elem` remaining = concat [downBranch, rightBranch]
-    | D `elem` remaining = downBranch
-    | R `elem` remaining = rightBranch
-    | otherwise = [[]]
-    where downBranch = [D:route | route <- routesLeft (delete D remaining)]
-          rightBranch = [R:route | route <- routesLeft (delete R remaining)]
-
-routes' :: Int -> Int -> [[Direction]]
-routes' x y = routesLeft initialDirections where
-    initialDirections = concat [(take x (repeat R)), (take y (repeat D))]
-
-main = print $ length (routes' 20 20)
+main = print $ numRoutes 20 20
